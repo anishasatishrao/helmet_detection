5ee1ed1253870ac0d0e621bf2a0e4ca0bacca269
Anisha Rao <85781358+anishasatishrao@users.noreply.github.com>
2025-08-15 20:46:07 +0530

    Update helmet_detect.py

--- helmet_detect.py
+++ helmet_detect.py
@@ -1,7 +1,7 @@
 # import cv2
 # from ultralytics import YOLO
 
-# model = YOLO("C:/Users/Anisha.S/Documents/helmet-detection-yolov8/venv/dataset/runs/detect/train2/weights/best.pt")
+# model = YOLO("best.pt")
 
 # cap = cv2.VideoCapture(0)  # or "video.mp4"
 
